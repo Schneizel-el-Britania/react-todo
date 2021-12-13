9190b29d1d28b18f48c3c18e140f02161923fd8d
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-12-13 13:08:55 +0200

    Set AddTask style

--- src/components/TodoList/TaskManager/AddTask/index.js
+++ src/components/TodoList/TaskManager/AddTask/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 
+import styles from './AddTask.module.scss';
+
 export default function AddTask() {
   return (
-    <form>
-      <input type="text" />
-      <AddBoxIcon />
+    <form className={styles.formContainer}>
+      <input type="text" className={styles.inputTask}/>
+      <AddBoxIcon className={styles.addBox}/>
     </form>
   )
 }
--- src/components/TodoList/TaskManager/index.js
+++ src/components/TodoList/TaskManager/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 import AddTask from './AddTask';
 import FilterTasks from './FilterTasks';
 
+import styles from './TaskManager.module.scss';
+
 export default function TaskManager() {
   return (
-    <article>
+    <article className={styles.container}>
       <AddTask />
       <FilterTasks />
     </article>
